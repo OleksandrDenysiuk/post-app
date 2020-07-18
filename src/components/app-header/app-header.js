30b04dda13576feda571fe5b3e99a04d0c1d381b
Oleksandr <denysiuk.oleksandr1997@gmail.com>
2020-07-18 18:07:34 +0200

    Add components

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Oleksandr Denysiuk</h1>
+            <h2>5 records, from their likes 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import PostListItem from '../post-list-item';
+
+import './post-list.css';
+
+
+const PostList = (props) => {
+    return (
+        <ul className="app-list list-group">
+            <PostListItem label='Going ro learn React' important/>
+            <PostListItem label='That is so good'/>
+            <PostListItem label='Ineed a break...'/>
+        </ul>
+    )
+}
+
+export default PostList;
